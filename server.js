258a507d218829a7c1a9e4f5ee77844e85deb318
unknown <mdabulkalamosman@gmail.com>
2026-07-13 11:13:37 +0600

    Add Home Route

--- server.js
+++ server.js
@@ -6,6 +6,28 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, {
     "Content-Type": "text/html",
   });
+  if (req.url === "/") {
+    res.end(`
+      <!DOCTYPE html>
+      <html>
+      <head>
+          <title>Home</title>
+      </head>
+      <body>
+          <h1>Home Page</h1>
+
+          <p>Welcome to our Simple HTTP Server.</p>
+
+          <nav>
+              <a href="/">Home</a> |
+              <a href="/about">About</a> |
+              <a href="/contact">Contact</a>
+          </nav>
+      </body>
+      </html>
+    `);
+  }
+
 });
 
 server.listen(PORT, () => {
