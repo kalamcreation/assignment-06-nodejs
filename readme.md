6e6d3cf3d7362a462f214cc32191a0442c6ac1f9
unknown <mdabulkalamosman@gmail.com>
2026-07-13 20:02:11 +0600

    Complete Assignment

---
+++ readme.md
@@ -0,0 +1,24 @@
+# Simple HTTP Server
+
+## Run Project
+
+```bash
+npm install
+```
+
+```bash
+node server.js
+```
+
+Server will run on
+
+http://localhost:5000
+
+## Routes
+
+| Route | Output |
+|-------|--------|
+| / | Home Page |
+| /about | About Page |
+| /contact | Contact Page |
+| Invalid Route | 404 Page Not Found |
---
+++ server.js
@@ -0,0 +1,96 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+
+    return res.end(`
+
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>Home</title>
+            </head>
+            <body>
+                <h1>Home Page</h1>
+                <p>Welcome to my website.</p>
+                <a href="/">Home</a> |
+                <a href="/about">About</a> |
+                <a href="/contact">Contact</a>
+            </body>
+            </html>
+
+        `);
+    }
+
+  if (req.url === "/about") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+    return res.end(`
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>About</title>
+            </head>
+            <body>
+                <h1>About Page</h1>
+                <p>This is the About page.</p>
+                <a href="/">Home</a> |
+                <a href="/about">About</a> |
+                <a href="/contact">Contact</a>
+            </body>
+            </html>
+        `);
+  }
+
+  else if (req.url === "/contact") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+    return res.end(`
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>Contact</title>
+            </head>
+            <body>
+                <h1>Contact Page</h1>
+                <p>This is the Contact page.</p>
+                <a href="/">Home</a> |
+                <a href="/about">About</a> |
+                <a href="/contact">Contact</a>
+
+
+            </body>
+            </html>
+        `);
+  }
+
+  // 404 Page
+
+  res.writeHead(404, {
+    "Content-Type": "text/html",
+  });
+
+  res.end(`
+
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>404</title>
+        </head>
+        <body>
+            <h1>404 - Page Not Found</h1>
+            <a href="/">Go Home</a>
+        </body>
+        </html>
+
+    `);
+});
+
+server.listen(5000, () => {
+  console.log("Server running at http://localhost:5000");
+});
